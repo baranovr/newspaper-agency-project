bd1f691e0a354d042c7ea351e59cd1d6387a5111
Ruslan <rusipbox@gmail.com>
2024-02-04 14:30:04 +0200

    created newspaper/urls.py and filled out models.py with basic models

--- newspaper/models.py
+++ newspaper/models.py
@@ -1,3 +1,46 @@
+from datetime import date
+
+from django.contrib.auth.models import AbstractUser
+
 from django.db import models
 
-# Create your models here.
+from newspaper_agency_project import settings
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Redactor(AbstractUser):
+    years_of_experience = models.IntegerField(null=True, blank=True)
+
+    class Meta:
+        verbose_name = "redactor"
+        verbose_name_plural = "redactors"
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} ({self.username})"
+
+
+class Newspaper(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    published_date = models.DateField(default=date.today)
+    topic = models.ForeignKey(
+        Topic,
+        on_delete=models.CASCADE,
+        related_name="newspapers"
+    )
+    publishers = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name="newspaper"
+    )
+
+    def __str__(self):
+        return f"{self.title}"
