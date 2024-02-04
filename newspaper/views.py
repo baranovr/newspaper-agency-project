dbe3d3f35964c2aeb75b6e752ad9d33f08aed63d
Ruslan <rusipbox@gmail.com>
2024-02-04 14:44:49 +0200

    created index method in views.py, created index.html in templates/newspaper

--- newspaper/views.py
+++ newspaper/views.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.decorators import login_required
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from newspaper.models import Newspaper, Topic, Redactor
+
+
+@login_required
+def index(request: HttpRequest) -> HttpResponse:
+    topics_count = Topic.objects.count()
+    newspapers_count = Newspaper.objects.count()
+    redactors_count = Redactor.objects.count()
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "topics_count": topics_count,
+        "newspapers_count": newspapers_count,
+        "redactors_count": redactors_count,
+        "num_visits": num_visits + 1
+    }
+
+    return render(
+        request,
+        "newspaper/index.html",
+        context=context
+    )
