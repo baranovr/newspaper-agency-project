8cd11334e332614bb289d1cef06afdaaa2a13a01
Ruslan <rusipbox@gmail.com>
2024-02-25 01:27:16 +0200

    added https://newspaper-agency-project.onrender.com to ALLOWED_HOST

--- newspaper_agency_project/settings.py
+++ newspaper_agency_project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don"t run with debug turned on in production!
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
-DEBUG = "RENDER" not in os.environ
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "https://newspaper-agency-project.onrender.com"]
 
 # Application definition
 
